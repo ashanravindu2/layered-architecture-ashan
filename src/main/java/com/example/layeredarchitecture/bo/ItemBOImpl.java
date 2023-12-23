b69064733102f16e6ecc07372576b8740093b618
ashanravindu2 <ravindulakshithaashan@gmail.com>
2023-12-23 13:21:46 +0530

    DAOFactory

--- src/main/java/com/example/layeredarchitecture/bo/ItemBOImpl.java
+++ src/main/java/com/example/layeredarchitecture/bo/ItemBOImpl.java
@@ -1,5 +1,7 @@
 package com.example.layeredarchitecture.bo;
 
+import com.example.layeredarchitecture.dao.DAOFactory;
+import com.example.layeredarchitecture.dao.custom.CustomerDAO;
 import com.example.layeredarchitecture.dao.custom.ItemDAO;
 import com.example.layeredarchitecture.dao.custom.impl.ItemDAOImpl;
 import com.example.layeredarchitecture.model.ItemDTO;
@@ -9,7 +11,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ItemBOImpl implements ItemBO {
-    ItemDAO itemDAO=new ItemDAOImpl();
+    ItemDAO itemDAO= (ItemDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ITEM);
 
     //item business logic example
     @Override
